6c510a44bf4d7bc6246ab2d840820b8e73e68e9b
GTeague <developer@ableant.com>
2020-10-05 15:16:06 -0500

    remove proxy. add prod API url.

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: "http://52.86.154.61:3001",
   headers: {
     "Content-type": "application/json",
   },
